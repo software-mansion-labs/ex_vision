5132eaa9dac416c7d400f4a68209c3be47a6e628
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2024-04-16 07:52:07 +0200

    Export mobilenetv3 to onnx

---
+++ python/exports/mobilenetv1.py
@@ -0,0 +1,35 @@
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
+import torch
+import json
+from pathlib import Path
+
+base_dir = Path("models/object-detection/mobilenet_v3_small")
+base_dir.mkdir(parents=True, exist_ok=True)
+
+model_file = base_dir / "model.onnx"
+categories_file = base_dir / "categories.json"
+
+weights = MobileNet_V3_Small_Weights.DEFAULT
+model = mobilenet_v3_small(weights=weights)
+model.eval()
+
+categories = weights.meta["categories"]
+transforms = weights.transforms()
+
+print(transforms)
+
+with open(categories_file, "w") as f:
+    json.dump(categories, f)
+
+onnx_input = torch.rand(1, 3, 224, 224)
+
+torch.onnx.export(
+    model,
+    onnx_input,
+    str(model_file),
+    verbose=False,
+    input_names=["input"],
+    output_names=["output"],
+    dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+    export_params=True,
+)
