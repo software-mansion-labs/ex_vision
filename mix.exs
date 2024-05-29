82f7bfe560402d416dcee82b48c41b8232c9ecb8
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2024-05-29 11:47:27 +0200

    Set version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExVision.Mixfile do
   use Mix.Project
 
-  @version "0.1.0-rc1"
+  @version "0.1.0"
   @github_url "https://github.com/membraneframework-labs/ex_vision/"
 
   def project do
