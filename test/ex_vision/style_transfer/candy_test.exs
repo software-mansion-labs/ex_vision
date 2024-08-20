2b9c03a794520f4646543b171071aaa4d29a1f94
Mateusz Kopcinski <kopcion@Mateuszs-MacBook-Pro.local>
2024-08-20 10:35:34 +0200

    fix tests, added missing gt files

--- test/ex_vision/style_transfer/candy_test.exs
+++ test/ex_vision/style_transfer/candy_test.exs
@@ -1,14 +1,14 @@
 defmodule Configuration do
   def configuration do
     %{
-      ExVision.StyleTransfer.CandyTest  => [module: ExVision.StyleTransfer.Candy, gt_file: "cat_candy.out"],
-      ExVision.StyleTransfer.CandyFastTest  => [module: ExVision.StyleTransfer.CandyFast, gt_file: "cat_candy_fast.out"],
-      ExVision.StyleTransfer.PrincessTest  => [module: ExVision.StyleTransfer.Princess, gt_file: "cat_princess.out"],
-      ExVision.StyleTransfer.PrincessFastTest  => [module: ExVision.StyleTransfer.PrincessFast, gt_file: "cat_princess_fast.out"],
-      ExVision.StyleTransfer.UdnieTest  => [module: ExVision.StyleTransfer.Udnie, gt_file: "cat_udnie.out"],
-      ExVision.StyleTransfer.UdnieFastTest  => [module: ExVision.StyleTransfer.UdnieFast, gt_file: "cat_udnie_fast.out"],
-      ExVision.StyleTransfer.MosaicTest  => [module: ExVision.StyleTransfer.Mosaic, gt_file: "cat_mosaic.out"],
-      ExVision.StyleTransfer.MosaicFastTest  => [module: ExVision.StyleTransfer.MosaicFast, gt_file: "cat_mosaic_fast.out"],
+      ExVision.StyleTransfer.CandyTest  => [module: ExVision.StyleTransfer.Candy, gt_file: "cat_candy.gt"],
+      ExVision.StyleTransfer.CandyFastTest  => [module: ExVision.StyleTransfer.CandyFast, gt_file: "cat_candy_fast.gt"],
+      ExVision.StyleTransfer.PrincessTest  => [module: ExVision.StyleTransfer.Princess, gt_file: "cat_princess.gt"],
+      ExVision.StyleTransfer.PrincessFastTest  => [module: ExVision.StyleTransfer.PrincessFast, gt_file: "cat_princess_fast.gt"],
+      ExVision.StyleTransfer.UdnieTest  => [module: ExVision.StyleTransfer.Udnie, gt_file: "cat_udnie.gt"],
+      ExVision.StyleTransfer.UdnieFastTest  => [module: ExVision.StyleTransfer.UdnieFast, gt_file: "cat_udnie_fast.gt"],
+      ExVision.StyleTransfer.MosaicTest  => [module: ExVision.StyleTransfer.Mosaic, gt_file: "cat_mosaic.gt"],
+      ExVision.StyleTransfer.MosaicFastTest  => [module: ExVision.StyleTransfer.MosaicFast, gt_file: "cat_mosaic_fast.gt"],
     }
   end
 end
